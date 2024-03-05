25fef715dc723d355e3ed916144c42ac18500c54
erenokur <32430029+erenokur@users.noreply.github.com>
2024-03-05 21:15:29 +0300

    drawingBook

--- codes/drawingBook.cpp
+++ codes/drawingBook.cpp
@@ -2,6 +2,15 @@
 
 using namespace std;
 
+/**
+* @brief This method finds the minimum number of pages that must be turned in order to get to a specific page.
+* @details This method calculates the minimum number of pages that must be turned in order to get to a specific page. 
+    It uses the formula min(p / 2, n / 2 - p / 2) to calculate the minimum number of pages that must be turned.
+    Function time complexity is O(1).
+* @param n The number of pages in the book.
+* @param p The page number to turn to.
+* @return The minimum number of pages that must be turned.
+*/
 int drawingBook(int n, int p) {
     return std::min(p / 2, n / 2 - p / 2);
 }
